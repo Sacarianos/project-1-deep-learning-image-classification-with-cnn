ccafe1e30a44ac9876967b605a2739359374510e
Sacarianos <jaime.velez1@upr.edu>
2024-12-20 10:01:24 -0400

    added final report

--- request.py
+++ request.py
@@ -2,10 +2,6 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import requests
 import json
-import io
-from google.auth import default
-from google.auth.transport.requests import Request
-import base64
 
 # Load and preprocess the image
 def preprocess_image(img_path, target_size=(32, 32)):
@@ -15,13 +11,8 @@ def preprocess_image(img_path, target_size=(32, 32)):
     img_array /= 255.0  # Normalize to [0, 1] range
     return img_array
 
-# Convert image to base64
-def image_to_base64(img_path):
-    with open(img_path, "rb") as img_file:
-        return base64.b64encode(img_file.read()).decode('utf-8')
-
 # Path to the image you want to predict
-img_path = 'images/360_F_177742846_umwpEr5OqwEQd4a9VyS7BGJX3tINNDe7.jpg'  # Replace with the path to your image
+img_path = './image-prediction/images/360_F_177742846_umwpEr5OqwEQd4a9VyS7BGJX3tINNDe7.jpg'  
 
 # Preprocess the image
 img_array = preprocess_image(img_path)
@@ -30,44 +21,16 @@ img_array = preprocess_image(img_path)
 input_data = np.expand_dims(img_array, axis=0).tolist()
 
 # Send the request to TensorFlow Serving
-endpoint_url = 'https://us-east1-aiplatform.googleapis.com/v1/projects/10609508497/locations/us-east1/endpoints/2868357556030406656:predict'
-
-# Authenticate using service account credentials
-credentials, project = default()
-credentials.refresh(Request())
-
-# Refresh the credentials (required if expired)
-if credentials.expired:
-    credentials.refresh(Request())
-
-# Obtain the access token
-access_token = credentials.token
-
-# Add the authorization header to the request
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {access_token}"
-}
-
-# Convert the image to base64 for the request (if needed)
-base64_image = image_to_base64(img_path)
-
-# Data structure for the request (with the base64 image)
-data = json.dumps({
-    "instances": [{"image": {"b64": base64_image}}]
-})
-
-# Make the prediction request
-response = requests.post(endpoint_url, data=data, headers=headers)
+data = json.dumps({"instances": input_data})
+headers = {"content-type": "application/json"}
+response = requests.post('http://localhost:8501/v1/models/my_model:predict', data=data, headers=headers)
 
 # Print the predicted class
 predictions = response.json()
-print(predictions)
 
 # Class names
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-# Get the prediction values
 prediction_values = predictions['predictions'][0]
 
 # Get the index of the class with the highest probability
@@ -78,3 +41,4 @@ predicted_class_name = class_names[predicted_class_index]
 
 # Print the predicted class name
 print(f"Predicted Class: {predicted_class_name}")
+
